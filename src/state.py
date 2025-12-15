00651a44b9a1ef498f1fd6a91b1ff992b6427f2e
ish4n10 <ishan.tripathi010@gmail.com>
2025-12-15 16:12:48 +0530

    Add Step function and LOCAL_x ops

--- src/state.py
+++ src/state.py
@@ -1,3 +1,5 @@
+import z3
+
 class State:
     def __init__(self):
         self.pc = 0
@@ -16,6 +18,31 @@ class State:
         new_state.history = list(self.history)
         return new_state
     
+    def step(self, program: list):
+        current_addr = program[self.pc]
+        current_op = current_addr[0]
+        arg = current_addr[1] if len(current_addr) > 1 else None
 
+        self.pc += 1
+        # let this be string for now 
+        match(current_op):
+            case "NOP":
+                return ("continue", self)
 
-
+            case "LOCAL_READ":
+                symbol = z3.BitVec(f"local_{arg}", 32)
+                self.sym_locals[arg] = symbol
+                return ('continue', self)
+            
+            case "LOCAL_GET":
+                value = self.sym_locals[arg]
+                self.sym_stack.append(value)
+                return ('continue', self)
+            
+            case "LOCAL_SET":
+                value = self.sym_stack.pop()
+                self.sym_locals[arg] = value
+                return ('continue', self)
+            
+            case _:
+                raise Exception(f"Unknown instruction: {current_op}")
